2962259a1e136b661b5304cf6c1a337cebe43bde
Paul Vidal <paul@tecactus.com>
2018-07-18 11:59:04 -0500

    changes

---
+++ resources/views/testimonio.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Toyota Yaris 2015</title>
+	<meta property="og:url"                content="{{ url()->current() }}" />
+	<meta property="og:type"               content="article" />
+	<meta property="og:title"              content="Toyota Yaris 2015" />
+	<meta property="og:description"        content="Mira el replay de esta increible subasta" />
+	<meta property="og:image"              content="{{ asset('img/testimonio.png') }}" />
+</head>
+<body>
+	<img src="{{ asset('img/testimonio.png') }}" alt="">
+	<script>
+		console.log('aca');
+	</script>
+</body>
+</html>
